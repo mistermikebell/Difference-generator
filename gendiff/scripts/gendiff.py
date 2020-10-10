46f8a6a8c8cd10ed6d02c44582e063d3db33787d
liuxindao <jokerbell7@gmail.com>
2020-10-10 19:38:59 +0300

    create positional arguments

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
