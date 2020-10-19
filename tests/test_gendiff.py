59349d955406f5387e56755f0c8cc50dc0a90a60
liuxindao <jokerbell7@gmail.com>
2020-10-19 23:04:14 +0300

    update task4 v6

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,10 +6,10 @@ from gendiff.create_diff_file import generate_diff
 
 
 def test_files_diff():
-    result = open("./tests/result.txt").read()
-    output = generate_diff("./tests/file1.json", "./tests/file2.json")
-    print("This is output:", output)
-    print("Result:", result)
+    result = open("result.txt").read()
+    output = generate_diff("file1.json", "file2.json")
+    print(output)
+    print(result)
     assert output == result
 
 test_files_diff()
