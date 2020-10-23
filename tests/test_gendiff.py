8e742b27fe88ab0d0559dfdebfa62295c20127b6
liuxindao <jokerbell7@gmail.com>
2020-10-23 21:29:32 +0300

    correct open function mistake

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,6 +6,6 @@ from gendiff.generate_diff import gen_diff
 
 
 def test_files_diff():
-    result = open("./tests/fixtures/result.txt").read()
+    result = open("./tests/fixtures/result.txt").read()[:-1]
     output = gen_diff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json")
     assert output == result
