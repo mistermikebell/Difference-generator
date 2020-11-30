a0aeceeff34c67fdb237c2584c6bc33196c0c256
mistermikebell <jokerbell7@gmail.com>
2020-11-30 21:12:21 +0300

    refactor sorting in tree builder

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -11,11 +11,11 @@ def build(before_dict, after_dict, sort_keys=True):
         return sorted(keys) if sort_keys else keys
 
     added_items = {key: (ADDED, after_dict[key])
-                   for key in sort(after_dict.keys() - before_dict.keys())}
+                   for key in after_dict.keys() - before_dict.keys()}
     removed_items = {key: (REMOVED, before_dict[key])
-                     for key in sort(before_dict.keys() - after_dict.keys())}
+                     for key in before_dict.keys() - after_dict.keys()}
     intersec = {}
-    for key in sort(before_dict.keys() & after_dict.keys()):
+    for key in before_dict.keys() & after_dict.keys():
         if isinstance(before_dict[key], dict) and isinstance(after_dict[key], dict):  # noqa: E501
             adjusted_value = build(before_dict[key], after_dict[key])
             intersec[key] = (NESTED, adjusted_value)
@@ -23,4 +23,4 @@ def build(before_dict, after_dict, sort_keys=True):
             intersec[key] = (UNCHANGED, before_dict[key])
         else:
             intersec[key] = (CHANGED, [before_dict[key], after_dict[key]])
-    return {**added_items, **removed_items, **intersec}
+    return dict(sort({**added_items, **removed_items, **intersec}.items()))
