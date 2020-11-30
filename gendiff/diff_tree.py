0709bb4d53d483c4eb4317cc0c66d56a89706814
mistermikebell <jokerbell7@gmail.com>
2020-11-30 17:20:05 +0300

    change function name

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -13,7 +13,7 @@ def build(before_dict, after_dict):
     intersec = {}
     for key in before_dict.keys() & after_dict.keys():
         if isinstance(before_dict[key], dict) and isinstance(after_dict[key], dict):  # noqa: E501
-            adjusted_value = compile(before_dict[key], after_dict[key])
+            adjusted_value = build(before_dict[key], after_dict[key])
             intersec[key] = (NESTED, adjusted_value)
         elif before_dict[key] == after_dict[key]:
             intersec[key] = (UNCHANGED, before_dict[key])
