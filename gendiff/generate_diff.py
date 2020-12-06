33f6a340aaab2431e0ed7380f2302a901a0a8ba8
mistermikebell <jokerbell7@gmail.com>
2020-12-06 22:10:46 +0300

    rename vars

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,8 +4,8 @@ from gendiff import diff_tree
 
 
 def generate_diff(before_file_path, after_file_path, formater='stylish'):
-    before_file = get_file_data(before_file_path)
-    after_file = get_file_data(after_file_path)
-    diff = diff_tree.build(before_file, after_file)
+    before_data = get_file_data(before_file_path)
+    after_data = get_file_data(after_file_path)
+    diff = diff_tree.build(before_data, after_data)
     transform_to_format = format.choose(formater)
     return transform_to_format(diff)
